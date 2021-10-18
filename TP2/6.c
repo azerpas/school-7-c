fff5ad0aadf8d66b833e25d1863ad35c947e67da
MAA6DY <fixed-term.anthony.manikhouth@bosch.com>
2021-10-18 16:32:06 +0200

    6

---
+++ TP2/6.c
@@ -0,0 +1,39 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define CONSTANT_PROC 352000000
+
+void do_work(unsigned int nb_secondes);
+
+void handler(){
+    printf("Doing work...\n");
+    alarm(1);
+    do_work(500);
+    signal(SIGALRM, handler);
+}
+
+int main(int argc, char const *argv[])
+{
+    signal(SIGALRM, handler);
+    alarm(1);
+    int n = 0;
+    while(1){ 
+        n++;
+    }
+    return 0;
+}
+
+void do_work(unsigned int nb_secondes)
+{
+	unsigned int i = CONSTANT_PROC * nb_secondes;
+
+	while(i>0)
+	{
+		asm volatile("nop");
+		i--;
+	}
+
+	return;
+}
